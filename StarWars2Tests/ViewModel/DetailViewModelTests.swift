32ab3a1c88de49fb124b225e5017385c23fa4c75
Devashree Shukla <shukladevashree963@gmail.com>
2022-01-28 13:32:10 +0530

    Detail view model tests added

--- StarWars2Tests/ViewModel/DetailViewModelTests.swift
+++ StarWars2Tests/ViewModel/DetailViewModelTests.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+@testable import StarWars2
+import XCTest
+
+class DetailViewModelTests: XCTestCase {
+    var viewModel: DetailViewModel!
+
+    override func setUp() {
+        viewModel = DetailViewModel()
+    }
+
+    func testDetailViewModelShouldNotBeNil() {
+        XCTAssertNotNil(viewModel, "Detail view model should not be nil")
+    }
+    
+    func testDetailViewModelItemsShouldNotBeNil() {
+        XCTAssertNotNil(viewModel.starWarsItem, "DetailViewModel items should not be empty")
+    }
+    
+    func testDefaultDetailViewModelShouldBePlanets() {
+        XCTAssertNotNil(viewModel.starWarsItem == .planets, "SDetailViewModel items should be palnets by default")
+    }
+    
+    func testDetailViewModelItemsForDetailsShouldNotBeNil() {
+        XCTAssertNotNil(viewModel.items, "Displayfields Items should have some values to display")
+    }
+    
+    func testDetailViewModelItemsForDetailsForPlanets() {
+        XCTAssertEqual(viewModel.items.count, DetailDisplayFields.getDisplayFields(type: .planets).count, "Displayfields Items should have some values to display for planets")
+        XCTAssertEqual(viewModel.items.count, 13, "Displayfields Items should have 13 to display for planets")
+    }
+    
+    func testDetailViewModelSelectedItemShouldNotBeNil() {
+        let planet = Planets()
+        viewModel.item = planet
+        XCTAssertNotNil(viewModel.item, "Items should not be empty")
+    }
+    
+    func testDetailViewModelTitleValueShouldNotBeNil() {
+        XCTAssertNotNil(viewModel.fields, "Fields should not be empty")
+    }
+    
+    func testDetailViewModelTitlesShouldNotBeNil() {
+        viewModel.fields.forEach {
+            XCTAssertNotNil($0.0, "Fields titles should not be empty")
+        }
+    }
+    
+    // TODO: Need to update this once core data save-retrive issue gets fixed
+//    func testDetailViewModelSelectedItemPeopleShouldBeFilmsNSSecureCoding() {
+//        let planet = Planets()
+//        viewModel.item = planet
+//        XCTAssertNotNil(viewModel.item, "Items should not be empty")
+//        planet.people?.peopleList = [Residents(), Residents()]
+//        viewModel.item = planet
+//
+//        XCTAssertNotNil(viewModel.item?.films as? FilmsNSSecureCoding, "Items should not be empty")
+//    }
+
+}
+
